152d7ad22a4e82c2ad699b0adce90c51b6f5bbac
JuMANocta <julien.mangeat@reseau-cd.net>
2021-11-18 13:01:50 +0100

    ajout de project index

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hello_world.urls')),
+    path('projects/', include('projects.urls')),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request,'project_index.html', context)
+
+def project_details(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request,'project_details.html', context)
